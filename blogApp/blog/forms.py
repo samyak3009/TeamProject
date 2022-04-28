705f9484848089e49e0b37e33628df591be25658
samyak3009 <samyak3009@gmail.com>
2022-04-28 16:23:32 +0530

    structure changes

--- blogApp/blog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Post
-
-
-class UploadForm(forms.ModelForm):
-
-    class Meta:
-        model = Post
-        fields = ['title', 'content', 'post_image']
