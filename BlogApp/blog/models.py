6c392394ee820af58177edcb57f94f17263fd183
anshay <anshay12345@gmail.com>
2022-05-02 00:27:13 +0530

    Few bugs in email and search removed by anshay

--- BlogApp/blog/models.py
+++ BlogApp/blog/models.py
@@ -32,6 +32,6 @@ class Comment(models.Model):
     post = models.ForeignKey(Post,related_name="comments", on_delete=models.CASCADE)
     name=models.CharField(max_length=255)
     body=models.TextField()
-    date_added=models.DateTimeField(auto_now_add=True)
+    date_added=models.DateTimeField(default=timezone.now)
     def __str__(self):
         return '%s - %s' % (self.post.title, self.name)
--- BlogApp/users/models.py
+++ BlogApp/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+User._meta.get_field('email')._unique = True
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
